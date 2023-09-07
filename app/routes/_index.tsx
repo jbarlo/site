eb4e89a24fff47c8e71a52427992f787da0d77ab
James Barlow <james-b-git@outlook.com>
2023-09-06 21:01:12 -0700

    Play around with font positioning

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,19 +6,56 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+const lightModePalette = { title: "black", subdomain: "gray", bg: "white" };
+const darkModePalette = { title: "white", subdomain: "gray", bg: "black" };
+const palette = true ? darkModePalette : lightModePalette;
+
 export default function Index() {
   return (
-    <div style={{ lineHeight: "1.8" }}>
-      <h1>
-        <span>
-          barlo<span style={{ color: "gray", fontStyle: "italic" }}>/www</span>
-        </span>
-      </h1>
-      <h1>
-        <span>
-          barlo<span style={{ color: "gray", fontStyle: "italic" }}>/g</span>
-        </span>
-      </h1>
+    <div style={{ backgroundColor: palette.bg }}>
+      <div
+        style={{
+          lineHeight: "1.8",
+          marginLeft: "20px",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <h1
+          style={{
+            margin: 0,
+            marginTop: "12px",
+            marginBottom: "20px",
+            lineHeight: 1,
+            color: palette.title,
+          }}
+        >
+          <span style={{ letterSpacing: "0.15em" }}>barlo</span>
+          <span style={{ color: palette.subdomain, fontStyle: "italic" }}>
+            <span
+              style={{
+                letterSpacing: "0.1em",
+                fontSize: "1.3em",
+                verticalAlign: "-12.5%",
+              }}
+            >
+              /
+            </span>
+            <span>www</span>
+          </span>
+        </h1>
+        <div
+          style={{
+            flexGrow: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ fontSize: "2em" }}>👀</div>
+        </div>
+      </div>
     </div>
   );
 }
