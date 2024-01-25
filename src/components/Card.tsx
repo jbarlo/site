95eab595594fa6dd335226be18534f8d3251e98a
James Barlow <james-b-git@outlook.com>
2024-01-24 19:20:05 -0800

    Fix polaroid card title polygon; clean up garage page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,30 +3,32 @@ interface CardProps {
   description?: string;
   imgUrl: string;
   imgAlt: string;
-  href: string;
+  href?: string;
 }
 const Card = ({ label, description, imgUrl, imgAlt, href }: CardProps) => (
   <a
     href={href}
     title={imgAlt}
-    className="text-limed-spruce-950 group aspect-square transition-transform hover:-translate-y-0.5 focus:-translate-y-0.5 motion-reduce:transform-none motion-reduce:transition-none"
+    className="group aspect-square text-limed-spruce-950 transition-transform hover:-translate-y-0.5 focus:-translate-y-0.5 motion-reduce:transform-none motion-reduce:transition-none"
   >
-    <div className="bg-limed-spruce-200 relative box-border h-full w-full overflow-hidden p-1">
+    <div className="relative box-border h-full w-full overflow-hidden bg-limed-spruce-200 p-1">
       <img
         src={imgUrl}
         alt={imgAlt}
-        className="bg-limed-spruce-300 saturate-60 h-full w-full max-w-none object-cover transition group-hover:saturate-100 group-focus:saturate-100"
+        className="h-full w-full max-w-none bg-limed-spruce-300 object-cover saturate-60 transition group-hover:saturate-100 group-focus:saturate-100"
       />
       <div className="absolute bottom-0 right-0 z-10 w-full transition-transform group-hover:-translate-y-1 group-focus:-translate-y-1 motion-reduce:transition-none">
         <div className="flex justify-end">
           <div
-            className="max-w-3/4 bg-limed-spruce-200 h-6 w-fit text-ellipsis pl-3 pr-3 text-right"
-            style={{ clipPath: "polygon(12% 0%, 100% 0%, 100% 100%, 0% 100%)" }}
+            className="max-w-3/4 h-6 w-fit text-ellipsis bg-limed-spruce-200 pl-3 pr-3 text-right"
+            style={{
+              clipPath: "polygon(10px 0%, 100% 0%, 100% 100%, 0% 100%)",
+            }}
           >
             {label}
           </div>
         </div>
-        <div className="bg-limed-spruce-200 h-8 w-full text-ellipsis pl-2 pr-3 text-right text-xs">
+        <div className="h-8 w-full text-ellipsis bg-limed-spruce-200 pl-2 pr-3 text-right text-xs">
           {description}
         </div>
       </div>
--- src/projects.ts
+++ src/projects.ts
@@ -8,61 +8,20 @@ const projects: {
   github?: string;
 }[] = [
   {
-    id: "test1",
-    label: "Test 1",
-    description: "is dog",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
-    id: "test2",
-    label: "Test 2",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
-    id: "test3",
-    label: "Test 3",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
-    id: "test1",
-    label: "Test 1",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
-    id: "test4",
-    label: "Test 4",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
-    id: "test3",
-    label: "Test 3",
-    lastUpdated: new Date(),
-    imgUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
-  },
-  {
     id: "site",
     label: "Site",
     lastUpdated: new Date(),
     imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
-    imgAlt: "is dog",
+    imgAlt: "a test dog",
+  },
+  {
+    id: "testdog",
+    label: "test dog please ignore",
+    lastUpdated: new Date(),
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg",
+    imgAlt: "a test dog",
   },
 ];
 
