7ad9caaeb07fa3ef7ba8aeb7c631cef3a1755668
James Barlow <james-b-git@outlook.com>
2024-02-21 19:26:58 -0800

    Switch header to non-condensed

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,14 +1,29 @@
 import type { FC } from "react";
+import "@fontsource/barlow";
 
 export interface HeaderTitleProps {
   suffix?: string;
 }
 
 const HeaderTitle: FC<HeaderTitleProps> = ({ suffix }: HeaderTitleProps) => (
-  <h1 className="text-title m-0 mb-1 mt-1 inline text-3xl font-normal leading-none">
-    <span style={{ letterSpacing: "0.15em" }}>barl</span>
-    <span style={{ letterSpacing: "0.06em" }}>o</span>
-    <span className="text-subdomain font-extralight italic">
+  <h1 className="m-0 mb-1 mt-1 inline text-3xl font-normal leading-none text-title">
+    <span
+      style={{
+        letterSpacing: "0.1em",
+        fontFamily: "Barlow, sans-serif",
+      }}
+    >
+      barl
+    </span>
+    <span
+      style={{
+        letterSpacing: "0.06em",
+        fontFamily: "Barlow, sans-serif",
+      }}
+    >
+      o
+    </span>
+    <span className="font-extralight italic text-subdomain">
       <span
         style={{
           letterSpacing: "0.04em",
